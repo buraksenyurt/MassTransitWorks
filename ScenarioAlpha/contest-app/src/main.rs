4fa723ca96f324e7181f532c6ec76049cf232753
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-10 22:45:43 +0300

    Refactored game codes.

--- ScenarioAlpha/contest-app/src/main.rs
+++ ScenarioAlpha/contest-app/src/main.rs
@@ -1,11 +1,17 @@
+use chrono::{DateTime, Utc};
+use lapin::options::{BasicPublishOptions, QueueDeclareOptions};
+use lapin::types::FieldTable;
+use lapin::Connection;
+use lapin::{BasicProperties, ConnectionProperties};
 use rand::prelude::SliceRandom;
 use rand::thread_rng;
-use serde::Deserialize;
-use serde_json::from_reader;
+use serde::{Deserialize, Serialize};
+use serde_json::{from_reader, to_string};
 use std::fs::File;
 use std::io::{stdin, stdout, Error, Write};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut game = Game::default();
 
     println!("Dünyanın en popüler coğrafya quiz yarışmasına hoşgeldiniz.");
@@ -14,10 +20,14 @@ fn main() {
     stdin()
         .read_line(&mut game.player_name)
         .expect("Okuma hatası.");
-    println!("Yarışmamız hoş geldin {}", game.player_name);
+    println!("Yarışmamıza hoş geldin {}", game.player_name);
     let mut questions = load_questions("data.json").expect("Sorular okunamadı");
     run_quiz(&mut questions, &mut game);
     println!("Yarışma sona erdi. Toplam puanın...{}", game.total_score);
+
+    if let Err(e) = send_message(game).await {
+        eprintln!("Failed to send message to RabbitMQ: {}", e);
+    }
 }
 
 #[derive(Default)]
@@ -60,4 +70,69 @@ fn load_questions(file_path: &str) -> Result<Vec<Question>, Error> {
     let file = File::open(file_path)?;
     let questions = from_reader(file)?;
     Ok(questions)
+}
+
+async fn send_message(game: Game) -> Result<(), Box<dyn std::error::Error>> {
+    let new_game_score = NewGameScore::new(
+        1,
+        18,
+        game.player_name.trim().to_string(),
+        game.total_score as f64,
+    );
+
+    let payload = to_string(&new_game_score)?.into_bytes();
+
+    let addr = "amqp://scothtiger:123456@localhost:5672/%2f";
+    let conn = Connection::connect(addr, ConnectionProperties::default()).await?;
+    let channel = conn.create_channel().await?;
+    let _queue = channel
+        .queue_declare(
+            "msg_gamers_score",
+            QueueDeclareOptions {
+                durable: true,
+                ..Default::default()
+            },
+            FieldTable::default(),
+        )
+        .await?;
+
+    channel
+        .basic_publish(
+            "",
+            "msg_gamers_score",
+            BasicPublishOptions::default(),
+            &payload,
+            BasicProperties::default(),
+        )
+        .await?
+        .await?;
+    println!("Sent message: {:?}", new_game_score);
+
+    Ok(())
+}
+
+#[derive(Serialize, Debug)]
+struct NewGameScore {
+    #[serde(rename = "PlayerID")]
+    player_id: i32,
+    #[serde(rename = "GameID")]
+    game_id: i32,
+    #[serde(rename = "Nickname")]
+    nickname: String,
+    #[serde(rename = "Point")]
+    point: f64,
+    #[serde(rename = "RecordTime")]
+    record_time: DateTime<Utc>,
+}
+
+impl NewGameScore {
+    fn new(player_id: i32, game_id: i32, nickname: String, point: f64) -> Self {
+        NewGameScore {
+            player_id,
+            game_id,
+            nickname,
+            point,
+            record_time: Utc::now(),
+        }
+    }
 }
