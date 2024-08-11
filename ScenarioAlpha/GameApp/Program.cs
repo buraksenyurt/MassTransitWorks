bfa0eb7475e151215648aaee70e29080b82aeadd
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-08-11 10:54:14 +0000

    Refactored logger

--- ScenarioAlpha/GameApp/Program.cs
+++ ScenarioAlpha/GameApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using CommonLib;
 using MassTransit;
+using Serilog;
 
 namespace GameApp;
 
@@ -8,39 +9,64 @@ class Program
 {
     static async Task Main()
     {
-        Game game = new()
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo
+            .Console(outputTemplate: "[{Timestamp:dd.MM.yyyy-HH:mm:ss} {Level:u4}] {Message:lj}{NewLine}{Exception}")
+            .CreateLogger();
+
+        Log.Information("Game App starting...");
+
+        try
         {
-            GameID = 16,
-            Name = "Geography Quiz"
-        };
-        Player player = new()
-        {
-            PlayerID = 23
-        };
-        Console.WriteLine("Welcome to the world's most popular geography quiz!");
-        Console.WriteLine("What is your name?");
-        player.Nickname = Console.ReadLine();
-        Console.WriteLine($"Welcome, {player.Nickname}\n");
-        var questions = LoadQuestions(Path.Combine(Environment.CurrentDirectory, "Questions.json"));
-        if (!questions.Any())
-        {
-            Console.WriteLine("Questions could not be loaded. Please contact the game developer.");
+            Game game = new()
+            {
+                GameID = 16,
+                Name = "Geography Quiz"
+            };
+            Player player = new()
+            {
+                PlayerID = 23
+            };
+            Log.Information("Game created with ID: {GameID} and Name: {GameName}", game.GameID, game.Name);
+
+            Console.WriteLine("Welcome to the world's most popular geography quiz!");
+            Console.WriteLine("What is your name?");
+            player.Nickname = Console.ReadLine();
+            Console.WriteLine($"Welcome, {player.Nickname}");
+
+            var questions = LoadQuestions(Path.Combine(Environment.CurrentDirectory, "Questions.json"));
+            if (!questions.Any())
+            {
+                Log.Error("No questions loaded. Exiting the game.");
+                Console.WriteLine("Questions could not be loaded. Please contact the game developer.");
+                return;
+            }
+
+            player.GameScore = RunGame(questions);
+
+            var newGameScore = new NewGameScore(
+                game.GameID,
+                player.PlayerID,
+                player.Nickname ?? "Anonymous",
+                player.GameScore,
+                DateTime.Now
+            );
+
+            Console.WriteLine($"The quiz is over, dear '{player.Nickname}'");
+            Console.WriteLine($"You scored a total of {newGameScore.Point} points.");
+            Console.WriteLine("See you next time.");
+
+            await PublishMessage(newGameScore);
         }
-        player.GameScore = RunGame(questions);
-
-        var newGameScore = new NewGameScore(
-            game.GameID,
-            player.PlayerID,
-            player.Nickname ?? "Anonymous",
-            player.GameScore,
-            DateTime.Now
-        );
-
-        Console.WriteLine($"The quiz is over, dear '{player.Nickname}'");
-        Console.WriteLine($"You scored a total of {newGameScore.Point} points.");
-        Console.WriteLine("See you next time.");
-
-        await PublishMessage(newGameScore);
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "An unhandled exception occurred.");
+        }
+        finally
+        {
+            Log.Information("Game App ending...");
+            Log.CloseAndFlush();
+        }
     }
 
     static IEnumerable<Question> LoadQuestions(string filePath)
@@ -51,6 +77,7 @@ class Program
             PropertyNameCaseInsensitive = true
         };
         var questions = JsonSerializer.Deserialize<IEnumerable<Question>>(data, options);
+        Log.Information("{Count} questions loaded.", questions?.Count());
         return questions ?? [];
     }
 
@@ -77,6 +104,7 @@ class Program
                 Console.WriteLine($"Sorry, that's incorrect. The correct answer was '{q.Answer}'.");
                 score -= 1;
             }
+            Console.WriteLine();
         }
         return score;
     }
@@ -97,7 +125,7 @@ class Program
         try
         {
             await busControl.Publish(newGameScore);
-            Console.WriteLine("NewGameScore message sent to RabbitMQ.");
+            Log.Information("NewGameScore message sent to RabbitMQ. GameID: {GameID}, PlayerID: {PlayerID}, Points: {Points}", newGameScore.GameID, newGameScore.PlayerID, newGameScore.Point);
         }
         finally
         {
